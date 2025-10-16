292cef102b508ee08b3978beef9d2dbe43125905
andy071811 <andy.johnson.1993@hotmail.co.uk>
2025-10-16 16:03:18 +0100

    style change and secret key removed

--- fifteen_words_project/settings.py
+++ fifteen_words_project/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get("SECRET_KEY",
-                         "django-insecure-p-m#mb!ekwo&w5d@9meur=36qz42^*22dwu$ghg@yb&cz+%pei"
-                        )
+SECRET_KEY = environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
